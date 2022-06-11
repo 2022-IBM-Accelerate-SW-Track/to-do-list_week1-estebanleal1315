7a6cfb02b2a2cf671f33bae3fb3cc526307d88bc
Esteban Leal <estebanleal@Estebans-MacBook-Air.local>
2022-06-10 23:34:44 -0500

    Project 1

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../component/todos.css";
+import {Card,CardContent,Grid,ListItemButton,ListItemText,Checkbox,} from "@mui/material";
+import { ButtonGroup } from "react-bootstrap";
 
 // 1. This component formats and returns the list of todos.
 // 2. Treat the question mark like an if statement.
@@ -13,23 +15,27 @@ const Todos = ({ todos }) => {
       todos.map((todo) => {
         return (
           <Grid key={todo.id}>
-            <Card>
+            <Card classNam="listCard">
               {/* Remember, we set the local state of this todo item when the user submits the form in 
               AddTodo.js. All we need to do is return the todo list item */}
-              <CardContent>
-                <span style={{ padding: "50px" }}>add todo content</span>
+              <CardContent className="listeItemButton">
+                <ListItemText>
+                  <span style={{ padding: "50px" }}>{todo.content}
+                    <Checkbox className="todoCheck"/>{todo.content}
+                  </span>
+                </ListItemText>
               </CardContent>
             </Card>
           </Grid>
         );
       })
     ) : (
-      <p>{/*Enter your message here if todo list is empty*/}</p>
+      <p>You have no todo's left</p>
     );
     // Lastly, return the todoList constant that we created above to show all of the items on the screen.
     return (
       <div className="todoCollection" style={{ padding: "10px" }}>
-        {/*call todo list here*/}
+        {todoList}
       </div>
     );
 };
